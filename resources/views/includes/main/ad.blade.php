01259b1436790348e14651521ee000a1c15faed2
EugeneKu <metis_ast@mail.ru>
2016-07-05 16:08:01 +0600

    Create offers block

--- resources/views/includes/main/ad.blade.php
+++ resources/views/includes/main/ad.blade.php
@@ -1,5 +1,6 @@
 {{-- AD block --}}
 <div id="offers">
+    <!-- AD top links -->
     <div class="offers-top">
         <div class="offers-region left">
             <a href="#">Выберите регион <i class="fa fa-angle-down"></i></a>
@@ -8,7 +9,9 @@
             <a href="#">Разместите свое объявление сюда</a>
         </div>
     </div>
-    <div class="offers-content">
+    <!-- AD main content -->
+    <div class="offers-content text-center">
+        <!-- AD1 -->
         <div class="offers-block">
             <div class="offers-rect">
                 <a href="#">
@@ -36,8 +39,87 @@
                 </a>
             </div>
         </div>
-        <div class="offers-block">2</div>
-        <div class="offers-block">3</div>
-        <div class="offers-block">4</div>
+        <!-- AD2 -->
+        <div class="offers-block">
+            <div class="offers-rect">
+                <a href="#">
+                    <img src="/img/offers/8.png">
+                </a>
+            </div>
+            <div class="offers-square">
+                <a href="#">
+                    <img src="/img/offers/2.png">
+                </a>
+            </div>
+            <div class="offers-square">
+                <a href="#">
+                    <img src="/img/offers/7.png">
+                </a>
+            </div>
+            <div class="offers-rect">
+                <a href="#">
+                    <img src="/img/offers/4.png">
+                </a>
+            </div>
+        </div>
+        <!-- AD3 -->
+        <div class="offers-block">
+            <div class="offers-square">
+                <a href="#">
+                    <img src="/img/offers/3.png">
+                </a>
+            </div>
+            <div class="offers-square">
+                <a href="#">
+                    <img src="/img/offers/2.png">
+                </a>
+            </div>
+            <div class="offers-square">
+                <a href="#">
+                    <img src="/img/offers/6.png">
+                </a>
+            </div>
+            <div class="offers-square">
+                <a href="#">
+                    <img src="/img/offers/7.png">
+                </a>
+            </div>
+            <div class="offers-rect">
+                <a href="#">
+                    <img src="/img/offers/1.png">
+                </a>
+            </div>
+        </div>
+        <!-- AD4 -->
+        <div class="offers-block">
+            <div class="offers-square">
+                <a href="#">
+                    <img src="/img/offers/5.png">
+                </a>
+            </div>
+            <div class="offers-square">
+                <a href="#">
+                    <img src="/img/offers/5.png">
+                </a>
+            </div>
+            <div class="offers-rect">
+                <a href="#">
+                    <img src="/img/offers/4.png">
+                </a>
+            </div>
+            <div class="offers-rect">
+                <a href="#">
+                    <img src="/img/offers/4.png">
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- AD footer -->
+    <div class="offers-footer">
+        <p class="text-center">Еще нет своей компании, но хотите что-то продать? Тут вы можете
+            <a href="#" class="offers-link">
+                <i class="fa fa-plus"></i> Подать бесплатное объявление
+            </a>
+        </p>
     </div>
 </div>
