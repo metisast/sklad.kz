f7a5a02d6db68cf3fbb2e59d0c6b41e31f68c060
EugeneKu <metis_ast@mail.ru>
2016-08-03 17:41:12 +0600

    Save commit

---
+++ database/migrations/2016_08_03_112804_create_industries_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIndustriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('industries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('main_category_id');
+            $table->string('name', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('industries');
+    }
+}
