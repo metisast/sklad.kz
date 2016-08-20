bd67acbee3f94c08d203ab653806cabb28c26e77
EugeneKu <metis_ast@mail.ru>
2016-08-20 17:49:48 +0600

    edit filter

--- public/js/filter1.js
+++ public/js/filter1.js
@@ -8,7 +8,36 @@
             type: 'post'
         });
 
+        /* Main varibles */
+        var filterSubLink = $('.filter-sub-link');
 
+        /* Render the filter btn */
+        var filterBtn = function(text){
+            var btn = $("<div>");
+            var i = $("<i>");
+            var span = $("<span>");
+
+            btn.addClass('filter-btn');
+            i.addClass('fa fa-bell');
+            span.text(text);
+
+            btn.append([i, span]);
+
+            return btn;
+        };
+
+        filterSubLink.click(function(){
+            //Parent block
+            var parentBlock = $(this).closest('.filter-sub');
+
+            //Hide parent filter
+            parentBlock.hide(200);
+
+            //Call filter button with params
+            parentBlock.parent().append(filterBtn($(this).text()));
+
+            return false;
+        });
 
     });
 })(jQuery);
